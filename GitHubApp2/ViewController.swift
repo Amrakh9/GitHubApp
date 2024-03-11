e5e9e8464f7b692fa559b81478647e72b85678a2
Amrakh9 <emrahamiraslanov@gmail.com>
2024-03-11 14:31:17 +0400

    Initial Commit

---
+++ GitHubApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubApp2
+//
+//  Created by Amrah on 11.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
