c6ac6c06c1d2546fd7c6a8872c061e295d932e7a
Amrakh9 <emrahamiraslanov@gmail.com>
2024-03-23 18:53:31 +0400

    We have rewrote code so its neat and now our error codes in another folder. Everything works ok

--- GitHubApp2/Utilities/ErrorMessage.swift
+++ GitHubApp2/Utilities/ErrorMessage.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+
+//This is
+enum GFError: String, Error{
+    case invalidUsername = "This user name created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
