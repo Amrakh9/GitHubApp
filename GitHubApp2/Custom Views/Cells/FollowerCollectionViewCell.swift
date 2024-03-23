8e1f41c3c01ea92c16c2160bb13b0a6bf88a2c37
Amrakh9 <emrahamiraslanov@gmail.com>
2024-03-23 22:36:56 +0400

    So we vreated profile cards when we search smb on our app

---
+++ GitHubApp2/Custom Views/Cells/FollowerCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  FollowerCollectionViewCell.swift
+//  GitHubApp2
+//
+//  Created by Amrah on 23.03.24.
+//
+
+import UIKit
+
+
+class FollowerCollectionViewCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    
+    
+    let avatarImage = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func set(follower: Follower){
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure(){
+        addSubview(avatarImage)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImage.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            avatarImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            avatarImage.widthAnchor.constraint(equalTo: avatarImage.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImage.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
---
+++ GitHubApp2/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubApp2
+//
+//  Created by Amrah on 23.03.24.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
