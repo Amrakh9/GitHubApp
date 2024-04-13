4d72ebfcc5dddc5c5f9ebd61f9b60ec8a2d8a4e9
Amrakh9 <emrahamiraslanov@gmail.com>
2024-04-13 11:13:37 +0400

    Implementing the UISearchController

---
+++ GitHubApp2/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyStateView.swift
+//  GitHubApp2
+//
+//  Created by Amrah on 13.04.24.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
