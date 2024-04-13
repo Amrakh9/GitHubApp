d6867a55378eb72d345f941cd8591a3fba815bd9
Amrakh9 <emrahamiraslanov@gmail.com>
2024-04-13 23:10:01 +0400

    We can see followers profile in modal view

---
+++ GitHubApp2/Custom Views/Labels/GFSecondaryTitleLabelVC.swift
@@ -0,0 +1,20 @@
+//
+//  GFSecondaryTitleLabelVC.swift
+//  GitHubApp2
+//
+//  Created by Amrah on 13.04.24.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabelVC: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GitHubApp2/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  GitHubApp2
+//
+//  Created by Amrah on 13.04.24.
+//
+
+import Foundation
