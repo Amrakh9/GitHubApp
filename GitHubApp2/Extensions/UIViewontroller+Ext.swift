31242819ca3b57d9a58767df573531762f487bd6
Amrakh9 <emrahamiraslanov@gmail.com>
2024-04-15 12:04:01 +0400

    Modal View (Icon issue resolved)

--- GitHubApp2/Extensions/UIViewontroller+Ext.swift
+++ GitHubApp2/Extensions/UIViewontroller+Ext.swift
@@ -49,6 +49,13 @@ extension UIViewController {
         }
         
     }
+    
+    //EmptyState View
+    func showEmptyStateView(with message: String, in view: UIView){
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
 }
 
  
--- GitHubApp2/Screens/FollowerListViewController.swift
+++ GitHubApp2/Screens/FollowerListViewController.swift
@@ -13,8 +13,10 @@ class FollowerListVC: UIViewController {
     
     var username: String!
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page = 1
     var hasMoreFollowers = true
+    var isSearching = false
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -22,6 +24,7 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
@@ -50,19 +53,40 @@ class FollowerListVC: UIViewController {
         collectionView.reloadData() // Reload data after setting layout
     }
     
+    //Search Controller
+    func configureSearchController(){
+        let searchController  = UISearchController()
+        searchController.searchResultsUpdater            = self
+        searchController.searchBar.delegate              = self
+        navigationItem.hidesSearchBarWhenScrolling       = false
+        searchController.searchBar.placeholder           = "Search for the username"
+        navigationItem.searchController                  = searchController
+    }
+    
     
     func getFollowers(username: String, page: Int) {
+        
         showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
             self.dismissLoadingView()
+            
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
                 //This is old one in which when we scroll down we will only see 100 users, meaning in list there only be 100 at once
                 //self.followers = followers
-                self.updateData()
+                
+                //Showing EmptyStateView if followers is Empty
+                if self.followers.isEmpty {
+                    let message = "This user does't have any followers. Go follow them 🌚"
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                        return
+                    }
+                }
+                self.updateData(on: self.followers)
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
@@ -72,6 +96,7 @@ class FollowerListVC: UIViewController {
     
     
     func configureDataSource() {
+        
         dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCollectionViewCell.reuseID, for: indexPath) as! FollowerCollectionViewCell
             cell.set(follower: follower)
@@ -80,15 +105,18 @@ class FollowerListVC: UIViewController {
     }
     
     
-    func updateData() {
+    func updateData(on followers: [Follower]) {
+        
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
+        
     }
 }
 //Pagination
 extension FollowerListVC: UICollectionViewDelegate{
+    
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         let offsetY = scrollView.contentOffset.y
         let contentHeight = scrollView.contentSize.height
@@ -99,5 +127,37 @@ extension FollowerListVC: UICollectionViewDelegate{
             page += 1
             getFollowers(username: username, page: page)
         }
+    }
+    // This is our firt step to make transition to user profile screen. We wrote didselet btw
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // W ? T : F
+        let activeArray     = isSearching ? filteredFollowers : followers
+        let follower        = activeArray[indexPath.item]
+        
+        let userInfoVC      = UserInfoVC()
+        userInfoVC.username = follower.login
+        
+        let navController   = UINavigationController(rootViewController: userInfoVC)
+        present(navController, animated: true)
+        
+    }
+    
+}
+
+// What this is doing?
+// It is letting ViewController to know that everytime I changed the search results in the search bar it is letting me know that smt is changed
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased())}
+        updateData(on: filteredFollowers)
+    }
+    
+    //When we tap cancel button it will show original followers array rather than filtered one
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+        updateData(on: followers)
     }
 }
