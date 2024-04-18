f74a1bce7efeed44f9576e1de0e5603bc5fa0935
Amrakh9 <emrahamiraslanov@gmail.com>
2024-04-18 23:12:31 +0400

    Just in case

---
+++ GitHubApp2/Custom Views/Views/GFItemInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  GFItemInfoView.swift
+//  GitHubApp2
+//
+//  Created by Amrah on 18.04.24.
+//
+
+import UIKit
+
+class GFItemInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
